8a46f079a328c0103542049aca138d2108b9a5e4
Darlene250 <d.ayinkamiy@alustudent.com>
2025-07-27 13:32:52 +0200

    Create test_library.py

---
+++ test_library.py
@@ -0,0 +1,6 @@
+# test_library.py
+# This file tests all the main features of the admin part of the library system.
+
+import unittest  # This is Python's built-in testing tool
+from unittest.mock import patch  # This helps us "pretend" user input and function results
+import admin  # We're testing functions from the admin.py file
